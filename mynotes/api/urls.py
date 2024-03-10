ca11140296cccc2ce525b94cb8e5a3d9e751dd35
Daichi Morihara <daichi16daichi04@icloud.com>
2024-03-10 14:31:24 +0900

    backend

--- mynotes/api/urls.py
+++ mynotes/api/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes")
+    path('', views.getRoutes, name="routes"),
+    path('notes/', views.getNotes, name="getNotes"),
+    path('notes/<str:pk>/', views.getNote, name="getNote")
 
 ]
